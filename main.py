ec01162b9e673df87b40a459af267dab3b81a08b
no_license <gamerdynasty@csu.fullerton.edu>
2025-05-14 22:51:22 -0700

    added movement
    added left and right movement. it's also constrained to stay within window

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame, sys
+from spaceship import Spaceship
 
 pygame.init()
 
@@ -12,14 +13,21 @@ pygame.display.set_caption("Python Space Invaders")
 
 clock = pygame.time.Clock()
 
+spaceship = Spaceship(SCREEN_WIDTH, SCREEN_HEIGHT)
+spaceship_group = pygame.sprite.GroupSingle()
+spaceship_group.add(spaceship)
+
 while True: 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    #Updating
+    spaceship_group.update()
 
     #Drawing
     screen.fill(GREY)
-    
+    spaceship_group.draw(screen)
+
     pygame.display.update()
     clock.tick(60)
--- spaceship.py
+++ spaceship.py
@@ -1,6 +1,29 @@
 import pygame
 
 class Spaceship(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, screen_width, screen_height):
         super().__init__()
-        self.image = pygame.image.load("C:\Users\gamer\Desktop\spaceinvaders2025\graphics\spaceship.png")
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.image = pygame.image.load("Graphics\spaceship.png")
+        self.rect = self.image.get_rect(midbottom = (self.screen_width/2, self.screen_height/2))
+        self.speed = 6
+
+    def get_user_input(self):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+        
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+    
+    def update(self):
+        self.get_user_input()
+        self.constrain_movement()
+
+    def constrain_movement(self):
+        if self.rect.right > self.screen_width:
+            self.rect.right = self.screen_width
+        if self.rect.left < 0:
+            self.rect.left = 0
